bc5667b44aa0074d346772dd9ab66483fa4439d0
lmlima <lmlima@users.noreply.github.com>
2018-04-04 17:54:57 -0300

    Update V15__cria_papel_para_usuarios_nao_logados.sql

--- src/main/resources/db/migration/V15__cria_papel_para_usuarios_nao_logados.sql
+++ src/main/resources/db/migration/V15__cria_papel_para_usuarios_nao_logados.sql
@@ -35,4 +35,4 @@ INSERT INTO public.papeis (nome) VALUES ('PONTO_FOCAL');
 INSERT INTO public.papeis (nome) VALUES ('PUBLICADOR');
 INSERT INTO public.papeis (nome) VALUES ('EDITOR');
 INSERT INTO public.papeis (nome) VALUES ('CIDADAO');
-INSERT INTO public.usuarios (cpf, senha, papel_id, servidor, habilitado, siorg, email_primario, nome) VALUES ('12312312312', '$2a$10$1O.BjADPpzYc2qm6c27U8ucMfZEhhHUALb/4TjiQMMbjoRgIqqizm', 1, true, true, 'http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/108999', 'jeank@thoughtworks.com', 'Editor de Serviços');
+INSERT INTO public.usuarios (cpf, senha, papel_id, servidor, habilitado, siorg, email_primario, nome) VALUES ('12312312312', '$2y$10$qN48GQbURMXYpctXNu01guYro03cTvQ9H8YpPCF.yCk697X0PPZH2', 1, true, true, 'http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/108999', 'jeank@thoughtworks.com', 'Editor de Serviços');
